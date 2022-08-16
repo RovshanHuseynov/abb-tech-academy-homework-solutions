ddeb856028c270212ede50e618565443bbfa028e
rovshan.huseynov <gitlab123!>
2022-08-16 16:08:18 +0400

    -- hw1 - Find target number

---
+++ src/main/java/homework1/App1.java
@@ -0,0 +1,59 @@
+package homework1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App1 {
+
+    public static void main(String[] args) {
+
+        int target = (int) (Math.random() * 100);
+        int[] guessedNumbers = new int[100];
+        Scanner in = new Scanner(System.in);
+        int guess, index = 0;
+
+        System.out.print("Please enter your name: ");
+        String name = in.nextLine();
+        System.out.println("Let the game begin!");
+        while (true) {
+            try {
+                System.out.print("Please enter your guessed number: ");
+                guess = in.nextInt();
+
+                guessedNumbers[index++] = guess;
+
+                if (guess > target) {
+                    System.out.println("Your number is too big. Please, try again.");
+                } else if (guess < target) {
+                    System.out.println("Your number is too small. Please, try again.");
+                } else {
+                    System.out.printf("Congratulations, {%s}!\n", name);
+                    System.out.print("Your numbers: ");
+
+                    sortArray(guessedNumbers);
+
+                    for (int i = 0; i < index; i++) {
+                        System.out.print(guessedNumbers[i] + ", ");
+                    }
+                    System.out.println();
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Only digits should be entered!");
+                break;
+            }
+        }
+    }
+
+    public static void sortArray(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/homework2/App2.java
@@ -0,0 +1,7 @@
+package homework2;
+
+public class App2 {
+    public static void main(String[] args) {
+        System.out.println("App2");
+    }
+}

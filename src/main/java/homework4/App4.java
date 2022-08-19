b8e45227af4ad9d92a3398232d06c8ebe27f035c
rovshan.huseynov <gitlab123!>
2022-08-19 23:11:40 +0400

    -- hw4 - Create POJO classes

---
+++ src/main/java/homework4/App4.java
@@ -0,0 +1,7 @@
+package homework4;
+
+public class App4 {
+    public static void main(String[] args) {
+        System.out.println("adasdas");
+    }
+}
---
+++ src/main/java/homework4/Human.java
@@ -0,0 +1,69 @@
+package homework4;
+
+
+public class Human {
+    public String name;
+    public String surname;
+    public int year;
+    public int iq;
+    public Pet pet;
+    public Human mother;
+    public Human father;
+    public String[][] schedule;    // day of the week, type of the activity
+
+    public Human(){
+    }
+
+    public Human(String name, String surname, int year){
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+    }
+
+    public Human(String name, String surname, int year, Human father, Human mother){
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.father = father;
+        this.mother = mother;
+    }
+
+    public Human(String name, String surname, int year, int iq, Pet pet, Human mother, Human father, String[][] schedule) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.iq = iq;
+        this.pet = pet;
+        this.mother = mother;
+        this.father = father;
+        this.schedule = schedule;
+    }
+
+    public void greetPet(){
+        System.out.println("Hello, " + pet.nickname);
+    }
+
+    public void describePet(){
+        // I have a [species], he is [age] years old, he is [very sly]>50/[almost not sly]<50
+        System.out.print("I have a " + pet.species + ", he is " + pet.age + " years old, he is ");
+
+        if(pet.trickLevel > 50){
+            System.out.println("very sly");
+        } else {
+            System.out.println("almost not sly");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", year=" + year +
+                ", iq=" + iq +
+                ", mother=" + mother.name + " " + mother.surname +
+                ", father=" + father.name + " " + father.surname +
+                ", pet=" + pet +
+                '}';
+    }
+}

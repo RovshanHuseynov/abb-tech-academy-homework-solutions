4508304833685d7208c51a21b97a80b0666d9ddf
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-08-25 23:20:20 +0400

    -- hw6 - call garbage collector

--- src/main/java/homework6/App6.java
+++ src/main/java/homework6/App6.java
@@ -1,7 +1,9 @@
 package homework6;
 
 public class App6 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) throws Throwable {
+        Family family1 = new Family(new Human(), new Human());
+        family1 = null;
+        System.gc();
     }
 }

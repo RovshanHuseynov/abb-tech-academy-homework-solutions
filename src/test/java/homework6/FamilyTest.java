50a5d01137374b46b9dfb6052b49b15e7c438a8b
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-09-01 23:11:23 +0400

    -- hw6 - implement test classes with JUnit4

--- src/test/java/homework6/FamilyTest.java
+++ src/test/java/homework6/FamilyTest.java
@@ -1,4 +1,16 @@
 package homework6;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FamilyTest {
+    @Test
+    public void firstTest(){
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void secondTest(){
+        Assert.assertTrue("hello", true);
+    }
 }

83850906435b95828cdb0a3c47655c30db167a41
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-09-02 23:54:32 +0400

    -- hw6 - implement test classes with JUnit5

--- src/test/java/homework6/FamilyTest.java
+++ src/test/java/homework6/FamilyTest.java
@@ -1,9 +1,17 @@
 package homework6;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class FamilyTest {
+    Family family1;
+    @BeforeEach
+    void init(){
+        System.out.println("begin");
+        family1 = new Family(new Human(), new Human());
+    }
+
     @Test
     public void firstTest(){
         Assert.assertTrue(true);

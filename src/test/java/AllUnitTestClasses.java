faf777783ed96722e815496abf52621456594f9f
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-09-03 13:32:58 +0400

    -- hw6 - add @Suite for running all Tests simultaneously

---
+++ src/test/java/AllUnitTestClasses.java
@@ -0,0 +1,8 @@
+import homework6.FamilyTest;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({FamilyTest.class})
+public class AllUnitTestClasses {
+}

0b7ff615eda852817cbf924a43e9a725b5f1bc8b
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-09-13 22:35:12 +0400

    -- hw9 - invoke FamilyService from FamilyController

--- src/main/java/homework9/FamilyController.java
+++ src/main/java/homework9/FamilyController.java
@@ -1,4 +1,9 @@
 package homework9;
 
 public class FamilyController {
+    private final FamilyService familyService;
+
+    public FamilyController(FamilyService familyService){
+        this.familyService = familyService;
+    }
 }

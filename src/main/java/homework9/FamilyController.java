1ddbf782b0d0dc70fc4c41c78e4aa249fdff3428
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-09-14 22:17:25 +0400

    -- hw9 - add methods into FamilyController class

--- src/main/java/homework9/FamilyController.java
+++ src/main/java/homework9/FamilyController.java
@@ -1,9 +1,15 @@
 package homework9;
 
+import java.util.List;
+
 public class FamilyController {
     private final FamilyService familyService;
 
     public FamilyController(FamilyService familyService){
         this.familyService = familyService;
+    }
+
+    public List<Family> getAllFamilies(){
+        return familyService.getAllFamilies();
     }
 }

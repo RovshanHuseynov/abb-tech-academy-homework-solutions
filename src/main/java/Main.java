f46956cb6626b735928b0f492e3f29ea252cb83b
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-10-19 01:50:00 +0400

    -- hw9 - improve getNumberOfDays()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getNumberOfDays(1));
+        System.out.println("1: " + getNumberOfDays(1));
+        System.out.println("2: " + getNumberOfDays(2));
+        System.out.println("3: " + getNumberOfDays(3));
+        System.out.println("4: " + getNumberOfDays(4));
+        System.out.println("5: " + getNumberOfDays(5));
+        System.out.println("6: " + getNumberOfDays(6));
+        System.out.println("7: " + getNumberOfDays(7));
+        System.out.println("8: " + getNumberOfDays(8));
+        System.out.println("9: " + getNumberOfDays(9));
+        System.out.println("10: " + getNumberOfDays(10));
+        System.out.println("11: " + getNumberOfDays(11));
+        System.out.println("12: " + getNumberOfDays(12));
     }
 
     public static int getNumberOfDays(int month){
-        if(month == 1) return 31;
-        else if(month == 2) return 28;
-        else if(month == 3) return 31;
-        else if(month == 4) return 30;
-        else if(month == 5) return 31;
-        else if(month == 6) return 30;
-        else if(month == 7) return 31;
-        else if(month == 8) return 31;
-        else if(month == 9) return 30;
-        else if(month == 10) return 31;
-        else if(month == 11) return 30;
-        else if(month == 12) return 31;
-        throw new IllegalArgumentException("wrong input");
+        if(month < 8)
+            return 30 + (month % 2);
+        else
+            return 31 - (month % 2);
     }
 }

90afbb05d1c7d145860b8e5b55a36e62351a3a4d
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-10-18 22:51:16 +0400

    -- hw9 - call up all the methods from FamilyController

--- src/main/java/homework9/App9.java
+++ src/main/java/homework9/App9.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class App9 {
     public static void main(String[] args) {
         List<Family> familyList = new ArrayList<>();
-        Human father = new Human("Rovshan", "Huseynov", 1994);
-        Human mother = new Human("Ayten", "Huseynova", 1995);
+        Human father1 = new Human("Rovshan", "Huseynov", 1994);
+        Human mother1 = new Human("Ayten", "Huseynova", 1995);
         Human child1 = new Human("Eli", "Huseynov", 2000);
         Human child2 = new Human("Veli", "Huseynov", 2005);
         Human child3 = new Human("Zeli", "Huseynov", 2010);
-        Family family1 = new Family(father, mother);
+        Family family1 = new Family(father1, mother1);
         family1.addChild(child1);
         family1.addChild(child2);
         family1.addChild(child3);
@@ -24,6 +24,20 @@ public class App9 {
         familyController.displayAllFamilies();
         familyController.deleteAllChildrenOlderThan(20);
         System.out.println("Family size after deletion");
+        familyController.displayAllFamilies();
+
+        familyController.getAllFamilies().forEach(System.out::println);
+        familyController.getFamiliesBiggerThan(1).forEach(System.out::println);
+        familyController.getFamiliesLessThan(1).forEach(System.out::println);
+        System.out.println("countFamiliesWithMemberNumber: " + familyController.countFamiliesWithMemberNumber(4));
+
+        Human father2 = new Human("Samir", "Veliyev", 1990);
+        Human mother2 = new Human("Kemale", "Eliyeva", 1992);
+        familyController.createNewFamily(father2, mother2);
+        System.out.println("countAllFamilies: " + familyController.count());
+        familyController.displayAllFamilies();
+        System.out.println("countAllFamilies: " + familyController.count());
+        familyController.deleteFamilyByIndex(1);
         familyController.displayAllFamilies();
     }
 }

71d2b6947db68f6b4d8eab7c0c2b8b823c0638c6
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-10-19 14:26:04 +0400

    -- hw9 - implement printArray() method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("11: " + getNumberOfDays(11));
         System.out.println("12: " + getNumberOfDays(12));*/
 
-        System.out.println("1: " + getFebruary(1));
+        /*System.out.println("1: " + getFebruary(1));
         System.out.println("2: " + getFebruary(2));
         System.out.println("3: " + getFebruary(3));
         System.out.println("4: " + getFebruary(4));
@@ -24,10 +24,16 @@ public class Main {
         System.out.println("9: " + getFebruary(9));
         System.out.println("10: " + getFebruary(10));
         System.out.println("11: " + getFebruary(11));
-        System.out.println("12: " + getFebruary(12));
+        System.out.println("12: " + getFebruary(12));*/
+
+        printArray(5, 10);
     }
 
-    public static int getNumberOfDays(int month){
+    public static int getNumberOfDays(int month) {
+        return 30 + (month + month / 8) % 2;
+
+        //return 30 + (month / 8) + (1 - 2 * (month / 8)) * (month % 2);
+
         /*if(month < 8)
             return 30 + 0 + 1 * (month % 2);
         else
@@ -35,17 +41,42 @@ public class Main {
 
         //return 30 + (month / 8) + (1 - 2 * (month / 8)) * (month % 2);
 
-        if(month == 2){
+        /*if(month == 2){
             return 30 + (month / 8) + (1 - 2 * (month / 8)) * (month % 2) - 2;
         } else {
             return 30 + (month / 8) + (1 - 2 * (month / 8)) * (month % 2);
-        }
+        }*/
     }
 
-    public static int getFebruary(int month){
-        if(month == 2)
+    public static int getFebruary(int month) {
+        if (month == 2)
             return 1;
         else
             return 0;
+    }
+
+    public static void printArray(int n, int m) {
+        int[][] array = new int[n][m];
+
+        int cnt = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                array[i][j] = cnt++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(i%2==0) {
+                for (int j = 0; j < m - 1; j++) {
+                    System.out.print(array[i][j] + " ");
+                }
+                System.out.println(array[i][m - 1]);
+            } else{
+                for (int j = m-1; j > 0; j--) {
+                    System.out.print(array[i][j] + " ");
+                }
+                System.out.println(array[i][0]);
+            }
+        }
     }
 }

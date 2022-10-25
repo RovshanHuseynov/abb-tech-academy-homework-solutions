b158664687492ecabcaf72c58a203aaa3932075a
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-10-25 22:29:14 +0400

    -- hw9 - add testing to FamilyServiceTest class

--- src/test/java/homework9/FamilyServiceTest.java
+++ src/test/java/homework9/FamilyServiceTest.java
@@ -1,14 +1,22 @@
 package homework9;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 class FamilyServiceTest {
 
     Human father1, mother1, child1, child2, child3, child4, father2, mother2;
     Pet pet1, pet2;
+    Family family1;
+    CollectionFamilyDao collectionFamilyDao;
+    FamilyService familyService;
+    FamilyController familyController;
 
     @BeforeEach
     void setUp() {
@@ -23,26 +31,40 @@ class FamilyServiceTest {
         pet1 = new Dog("Alabash", 1, 100,
                 new HashSet<String>(){{add("jump"); add("lick"); add("sleep");}});
         pet2 = new DomesticCat("Luna");
+
+        family1 = new Family(father1, mother1);
+        family1.addChild(child1);
+        family1.addChild(child2);
+        family1.addChild(child3);
+        List<Family> familyList = new ArrayList<>();
+        familyList.add(family1);
+
+        collectionFamilyDao = new CollectionFamilyDao(familyList);
+        familyService = new FamilyService(collectionFamilyDao);
+        familyController = new FamilyController(familyService);
     }
 
     @Test
     void getAllFamilies() {
-    }
-
-    @Test
-    void displayAllFamilies() {
+        List<Family> excepted = Arrays.asList(family1);
+        Assertions.assertEquals(excepted, familyController.getAllFamilies());
     }
 
     @Test
     void getFamiliesBiggerThan() {
+        List<Family> excepted = Arrays.asList(family1);
+        Assertions.assertEquals(excepted, familyController.getFamiliesBiggerThan(1));
     }
 
     @Test
     void getFamiliesLessThan() {
+        List<Family> excepted = new ArrayList<>();
+        Assertions.assertEquals(excepted, familyController.getFamiliesLessThan(1));
     }
 
     @Test
     void countFamiliesWithMemberNumber() {
+        Assertions.assertEquals(1, familyController.countFamiliesWithMemberNumber(5));
     }
 
     @Test
